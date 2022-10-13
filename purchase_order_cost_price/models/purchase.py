0c2e30ccd4fa9a8e178ef41380ed753dad87a07d
dani <dani.xtendoo@gmail.com>
2022-10-13 09:55:43 +0200

    sync

---
+++ purchase_order_cost_price/models/purchase.py
@@ -0,0 +1,43 @@
+# -- coding: utf-8 --
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+from odoo.tools.float_utils import float_compare
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.osv import expression
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo.tools.float_utils import float_compare
+from odoo.exceptions import UserError, AccessError
+from odoo.tools.misc import formatLang
+from odoo.addons import decimal_precision as dp
+
+import logging
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        result = super(PurchaseOrderLine, self).onchange_product_id()
+        logging.info('***** Precio venta *****')
+        logging.info( self.price_unit)
+        #if self.price_unit == 0.00:
+        self.price_unit = self.product_id.standard_price
+
+        return result
+
+    @api.onchange('product_qty', 'product_uom')
+    def _onchange_quantity(self):
+        result = super(PurchaseOrderLine, self)._onchange_quantity()
+        logging.info('***** Precio venta df2 *****')
+        logging.info(self.price_unit)
+        #if self.price_unit == 0.00:
+        self.price_unit = self.product_id.standard_price
+
+        logging.info(self.price_unit)
+
+        return result
